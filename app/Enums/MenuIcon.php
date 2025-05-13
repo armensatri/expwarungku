2e60cebdc3aed3a616c2c76876423e2168198a8b
armensatri <belajarcoding444@gmail.com>
2025-05-14 00:08:14 +0700

    update

--- app/Enums/MenuIcon.php
+++ app/Enums/MenuIcon.php
@@ -6,13 +6,34 @@ class MenuIcon
 {
   public static function get(string $name): string
   {
+
     $icons = [
+      // DASHBOARD
       'owner' => 'dashboard.jpg',
       'superadmin' => 'dashboard.jpg',
-      'member' => 'dashboard.jpg'
+      'admin' => 'dashboard.jpg',
+      'member' => 'dashboard.jpg',
+
+      // ACCOUNT
+      'profile' => 'profile.jpg',
+      'profile edit' => 'profile-edit.png',
+      'change password' => 'change-password.jpg',
+
+      // MANAGEDATA
+      'data' => 'data.png',
+      'access' => 'access.png',
+
+      // MANAGEUSER
+      'users' => 'users.jpg',
+      'roles' => 'roles.jpg',
+      'permissions' => 'permissions.jpg',
+
+      // MANAGEMENU
+      'menus' => 'menus.jpg',
+      'submenus' => 'submenus.jpg',
     ];
 
     $filename = $icons[strtolower($name)] ?? '';
-    return "/backend/img/menu{$filename}";
+    return "/backend/img/menu/{$filename}";
   }
 }
